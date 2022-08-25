f413f1fdbdcdb93cfda1e7558d34665cafc520ce
DanCan30 <danielecan3008@gmail.com>
2022-08-25 15:08:09 +0200

    Pure PHP solution done

--- php/index.php
+++ php/index.php
@@ -5,16 +5,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dischi</title>
-    <link rel="stylesheet" href="../stiles/style.css">
+    <link rel="stylesheet" href="../assets/stiles/style.css">
 </head>
 <body>
 
+    <?php include "../db.php"?>
+
     <header>
-        <img src="../img/Spotify-logo.png" alt="Spotify Logo" id="logo">
+        <img src="../assets/img/Spotify-logo.png" alt="Spotify Logo" id="logo">
     </header>
 
     <main class="container">
 
+        <?php foreach($disks as $disk) {?>
+
+            <div class="disk">
+
+                <div class="img-container">
+                    <?php echo "<img src='" . $disk['poster'] . "' alt='" . $disk["title"] . "'>"?>
+                    <span>
+                        <?php echo $disk["genre"]?>
+                    </span>
+                </div>
+
+                <h2>
+                    <?php echo $disk["title"]?>
+                </h2>
+                <p>
+                    <?php echo $disk["author"]?>
+                </p>
+                <p>
+                    <?php echo $disk["year"]?>
+                </p>
+            </div>
+
+
+        <?php } ?>
+        
     </main>
 
 </body>
